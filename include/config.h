959a1e7aed9d88c6d0a88116da33997481bdf714
Ivan Makhotin <ivmakhotin@gmail.com>
2022-10-06 21:13:17 +0200

    refactored a bit

--- include/config.h
+++ include/config.h
@@ -7,9 +7,11 @@ const std::string DOOR_ID = "door_1";
 const std::string TG_TOKEN = "5632355593:AAGKK4XW7nqxn9j1XdTGP-DRIptTjM83PQo";
 const std::string CHAT_ID = "215497907";
 
-const int CRITICAL_TIME = 60*60;
-const int WAITING_TIME = 5*60;
+const int CRITICAL_TIME = 60 * 60;
+const int WAITING_TIME = 5 * 60;
 const int64_t START_TIMESTAMP = 1665022334943969;
 
-const std::string CRITICAL_TIME_EXCEEDED_MSG = "It doesn't work for long period. Last data point was at: ";
-const std::string IT_WORKS_AGAIN_MSG = "It works again! Last data point was at: ";
+const std::string CRITICAL_TIME_EXCEEDED_MSG =
+    "It doesn't work for long period. Last data point was at: ";
+const std::string IT_WORKS_AGAIN_MSG =
+    "It works again! Last data point was at: ";
--- include/exceptions.h
+++ include/exceptions.h
@@ -1,10 +1,9 @@
 #include <exception>
-#include <string>
 #include <stdexcept>
-
+#include <string>
 
 class RequestError : public std::runtime_error {
-public:
-    explicit RequestError(const std::string& what_arg):
-        std::runtime_error("RequestError: " + what_arg) {};
+  public:
+    explicit RequestError(const std::string &what_arg)
+        : std::runtime_error("RequestError: " + what_arg){};
 };
